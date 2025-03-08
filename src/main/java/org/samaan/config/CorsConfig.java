bce66b657474c8d619a6aacb38075d060da8f718
Preet Brahmbhatt <138224316+preet1694@users.noreply.github.com>
2025-03-08 05:41:58 +0530

    Update CorsConfig.java

--- src/main/java/org/samaan/config/CorsConfig.java
+++ src/main/java/org/samaan/config/CorsConfig.java
@@ -14,6 +14,8 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("https://samaan-rho.vercel.app") // Replace with your Vercel frontend URL
+                        .allowedOrigins("samaan-git-main-preet1694s-projects.vercel.app")
+                        .allowedOrigins("samaan-o32wuxevv-preet1694s-projects.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
